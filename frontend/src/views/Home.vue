c7419b76ccc455463ae841034cfa7bf1612ae08c
Anna Shokhireva <anna.shokhireva@iths.se>
2021-02-23 15:01:21 +0100

    Added logo to Home page

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/sinus-logo.svg">
+		<HelloWorld msg="Welcome to Sinus Webshop"/>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+	import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: 'Home',
+		components: {
+			HelloWorld
+		}
+	}
 </script>
